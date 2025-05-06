1a22951805156a2fda75e7649bce03ef3111c012
oeyoews <jyao4783@gmail.com>
2025-05-06 20:02:18 +0800

    init: add vue start kit

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    minify: false,
+    rollupOptions: {
+      external: ['vue'], // 将 Vue 标记为外部依赖
+      output: {
+        entryFileNames: 'app.js',
+        chunkFileNames: 'app-chunk.js',
+        assetFileNames: 'app.[ext]',
+        // manualChunks: {}, // 禁用代码分割
+        format: 'iife',
+        // 定义外部依赖的全局变量名
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    },
+    cssCodeSplit: false // 禁用 CSS 代码分割，所有 CSS 将被提取到一个文件中
+  }
+})
+
+
